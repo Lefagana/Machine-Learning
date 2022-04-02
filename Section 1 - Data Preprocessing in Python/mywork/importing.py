9bf609f972f8f1023140dcbfbde76877337b30d2
lefagana <malahmusalg@gmail.com>
2022-04-02 14:52:05 +0100

    commneting some code

--- Section 1 - Data Preprocessing in Python/mywork/importing.py
+++ Section 1 - Data Preprocessing in Python/mywork/importing.py
@@ -20,14 +20,15 @@ y = dataset.iloc[:,3].values
 #the reasons we used the NaN as a missing value is bcos our data has a NaN value
 #axis = 0 is for column and 1 is for rows
 #for age
-from sklearn.impute import SimpleImputer
+'''from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean') 
 # the upper bound is included thats why it dosnt start with index 0
 imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
-
+'''
 
 # Encoding categorical data
+'''
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 #for country
@@ -38,15 +39,15 @@ x = np.array(ct.fit_transform(x))
 #for purchase
 le = LabelEncoder()
 y = le.fit_transform(y) 
-
+'''
 #Splitting dataset into training set and test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 #feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 x_train = sc_X.fit_transform(x_train)
 x_test = sc_X.transform(x_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train.reshape(-1,1))
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
